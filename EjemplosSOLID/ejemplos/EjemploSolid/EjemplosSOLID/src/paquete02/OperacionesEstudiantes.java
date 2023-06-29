e7e1379d32c5fa99ea609a31ae189929c1c61aa2
Alex R <alexis.rj2110@gmail.com>
2023-06-29 17:24:48 -0500

    Subida 1

--- EjemplosSOLID/ejemplos/EjemploSolid/EjemplosSOLID/src/paquete02/OperacionesEstudiantes.java
+++ EjemplosSOLID/ejemplos/EjemploSolid/EjemplosSOLID/src/paquete02/OperacionesEstudiantes.java
@@ -11,31 +11,77 @@ import java.util.ArrayList;
  * @author reroes
  */
 public class OperacionesEstudiantes {
+
     private ArrayList<Persona> estudiantes;
     private double promedioEdades;
-    // private double edadminima;
-    
-    
-    public void establecerEstudiante(ArrayList<Persona> lista){
+    private int edadminima;
+    private int edadmaxima;
+    private String listaCiudadesEstudiantes;
+
+    public void establecerEstudiante(ArrayList<Persona> lista) {
         estudiantes = lista;
     }
-    
-    public ArrayList<Persona> obtenerEstudiante(){
+
+    public ArrayList<Persona> obtenerEstudiante() {
         return estudiantes;
     }
-    
-    public void establecerPromedioEdades(){
+
+    public void establecerPromedioEdades() {
         double suma = 0;
-        for(Persona e: obtenerEstudiante()){
-            suma = e.obtenerEdad();
+        for (Persona e : obtenerEstudiante()) {
+            suma = suma + e.obtenerEdad();
         }
-        promedioEdades = suma/obtenerEstudiante().size();
+        promedioEdades = suma / obtenerEstudiante().size();
     }
-    
-    public double obtenerPromedioEdades(){
-        
+
+    public double obtenerPromedioEdades() {
         return promedioEdades;
     }
+
+    public void establecerEdadMinima() {
+
+        int minimo = estudiantes.get(0).obtenerEdad();
+
+        for (Persona e : estudiantes) {
+            if (e.obtenerEdad() < minimo) {
+                minimo = e.obtenerEdad();
+            }
+        }
+
+        edadminima = minimo;
+    }
+
+    public int obtenerEdadMinima() {
+        return edadminima;
+    }
+
+    public void establecerEdadMaxima() {
+        int maximo = estudiantes.get(0).obtenerEdad();
+
+        for (Persona e : estudiantes) {
+            if (e.obtenerEdad() > maximo) {
+                maximo = e.obtenerEdad();
+            }
+        }
+
+        edadmaxima = maximo;
+    }
+
+    public int obtenerEdadMaxima() {
+        return edadmaxima;
+    }
     
-    
+    public void establecerListaCiudadesEstudiantes() {
+        String mensaje = "";
+        for (Persona e : estudiantes) {
+            mensaje = String.format("%s%s\n",
+                    mensaje,e.obtenerCiudad().obtenerNombreCiudad());
+        }
+        listaCiudadesEstudiantes = mensaje;
+    }
+
+    public String obtenerListaCiudadesEstudiantes() {
+        return listaCiudadesEstudiantes;
+    }
+
 }
--- EjemplosSOLID/ejemplos/EjemploSolid/EjemplosSOLID/src/paquete02/Principal.java
+++ EjemplosSOLID/ejemplos/EjemploSolid/EjemplosSOLID/src/paquete02/Principal.java
@@ -5,9 +5,38 @@
  */
 package paquete02;
 
+import java.util.ArrayList;
 
 public class Principal {
+
     public static void main(String[] args) {
-        
+
+        ArrayList<Persona> lista = new ArrayList();
+
+        Ciudad c1 = new Ciudad("Quito");
+        Ciudad c2 = new Ciudad("Loja");
+
+        Persona p1 = new Persona("Alex R", 18, c1);
+        Persona p2 = new Persona("Carlos Mejia", 19, c2);
+
+        lista.add(p1);
+        lista.add(p2);
+
+        OperacionesEstudiantes op1 = new OperacionesEstudiantes();
+
+        op1.establecerEstudiante(lista);
+        op1.establecerPromedioEdades();
+        op1.establecerEdadMinima();
+        op1.establecerEdadMaxima();
+        op1.establecerListaCiudadesEstudiantes();
+        System.out.printf("El promedio de edades es: %.2f\n",
+                op1.obtenerPromedioEdades());
+        System.out.printf("La edad minima es: %d\n",
+                op1.obtenerEdadMinima());
+        System.out.printf("La edad maxima es: %d\n",
+                op1.obtenerEdadMaxima());
+        System.out.printf("\nLa lista de ciudades es:\n%s\n",
+                op1.obtenerListaCiudadesEstudiantes());
+
     }
 }
